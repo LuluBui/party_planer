d5c95a38b8c240a5587a9c2b4fea1dab7392292f
LuluBui <141107536+LuluBui@users.noreply.github.com>
2023-08-28 15:40:32 -0400

    Update javascript.js
    removed logs

--- javascript.js
+++ javascript.js
@@ -1,20 +1,15 @@
 
 const list = document.querySelector('ul');
-//const events = getEventData();
-//console.log(events);
 render();
 
 async function getEventData(){
     const response = await fetch('https://fsa-crud-2aa9294fe819.herokuapp.com/api/2307-ftb-et-web-ft/events');
-    // console.log(response);
     const jdata = await response.json();
-    console.log(jdata.data);
     return jdata.data;
-    //return data;
 }
 
 async function render(){
-    events = await getEventData();
+    const events = await getEventData();
     const update = events.map(function(i){
 
         return `
